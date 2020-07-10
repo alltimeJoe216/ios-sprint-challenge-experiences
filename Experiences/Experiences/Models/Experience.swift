bbc568042a50a7aa43ea57ab684f16d8b851b3bc
Joe <squirlyproductions@gmail.com>
2020-07-10 11:40:28 -0400

    UI complete I think? Added Experience/Media Models. Group folders for organization

---
+++ Experiences/Experiences/Models/Experience.swift
@@ -0,0 +1,9 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by Joe Veverka on 7/10/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import Foundation
---
+++ Experiences/Experiences/Models/Media.swift
@@ -0,0 +1,9 @@
+//
+//  Media.swift
+//  Experiences
+//
+//  Created by Joe Veverka on 7/10/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import Foundation
